77018df10e436acf8394ffe8e3c090b192bf239c
Kevin Costello <kevincostello213@gmail.com>
2016-09-22 21:34:31 -0700

    Removed debugging printfs

--- ReverseStr.c
+++ ReverseStr.c
@@ -1,34 +1,36 @@
+/* 
+ * Author: Kevin Costello 
+ * Date: 9/22/2016 
+ */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 
 char *reverseStr(char *str) {
-	printf("%X", (unsigned int)str);
 	char *left = str;
 	char *right;
 	char temp;
 
 	while (*str != '\0') {
 		str++;
-	} // Move str past end of string
+	} // Move str to terminating '\0'
 	
 	// Set right to the last character in str
 	right = str - 1;
-	
-	printf("%c", *right);
+
 	// Reset str to original value
 	str = left;
-	printf("%s\n", str);
 
 	while (left < right) {
-		printf("%c", *left);
 		temp = *left;
 		*left = *right;
 		*right = temp;
+		//Move left pointer forward up string
 		++left;
+		// Right pointer moves backwards
 		--right;
 	}
 
-	printf("%X", (unsigned int)str);
 	return str;
 }
 
@@ -38,9 +40,9 @@ int main(int argc, char **args) {
 	printf("Enter a string less than 20 characters:\n");
 	scanf("%s", input);
 
-	input = reverseStr(input);
+	reverseStr(input);
 
-	printf("Done Reversing! Here is the result! :");
+	printf("Done Reversing! Here is the result! :\n");
 	printf("%s\n", input);
 
 	return 0;

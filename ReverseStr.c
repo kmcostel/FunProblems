27f5c96a8331e757865ababab672fe93bbba3dcc
Kevin Costello <Kevin@Kevins-MacBook-Pro.local>
2016-09-22 21:00:58 -0700

    Added gitignore and basic beginning of c file

---
+++ ReverseStr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **args) {
+
+	char input[20];
+	printf("Enter a string less than 20 characters:\n");
+	scanf("%s", input);
+	printf("%s\n", input);
+
+	return 0;
+}

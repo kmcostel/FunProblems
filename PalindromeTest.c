b05acd3fb707d1fbccc781636c5bb89253243910
Kevin Costello <kevincostello213@gmail.com>
2016-09-23 17:54:43 -0700

    Pre palindrome logic

---
+++ PalindromeTest.c
@@ -0,0 +1,27 @@
+/* Author: Kevin Costello
+ * Date: 9/23/2016
+ */
+
+ #include <stdio.h>
+
+int isPalindrome(char *str) {
+
+	return 0;
+}
+
+
+int main(int argc, char **argv) {
+	char testStr[20];
+
+	printf("Enter a string\n!");
+	scanf("%s", testStr);
+
+	if (isPalindrome(testStr)) {
+		printf("Yea that's a palindrome!\n");
+	}
+	else {
+		printf("Sorry! Not a palindrome!");
+	}
+
+	return 0;
+}

cf49c71538bc8519a173b620b021458ea7f32aed
Kevin Costello <kevincostello213@gmail.com>
2016-10-09 11:07:59 -0700

    Creating .gitignore

---
+++ Security/DiagonalCipher/DiagonalCipher.java
@@ -0,0 +1,15 @@
+/*
+   Author: Kevin Costello
+   Assignment #2 for CPE321
+   Due Date: 10/13/2016
+*/
+
+public class DiagonalCipher {
+   
+   public static void main (String args[]) {
+
+
+   }
+
+
+}

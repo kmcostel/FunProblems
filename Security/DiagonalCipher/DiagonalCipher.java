43581991279fcd1613d7bf7d48506c208fae5f4e
Kevin Costello <kevincostello213@gmail.com>
2016-10-10 07:42:38 -0700

    Adding README and sample output to end of source code

--- Security/DiagonalCipher/DiagonalCipher.java
+++ Security/DiagonalCipher/DiagonalCipher.java
@@ -15,14 +15,19 @@ public class DiagonalCipher {
   // index 1 is the file's second line and so on.
   private static ArrayList<String> lineList;
 
+  // Prints out the matrix
   private static void printMatrix(char[][] matrix) {
     String currLine;
+    // Formatting, add space before printing the matrix out
+    System.out.println();
     for (int r = 0; r < matrix.length; r++) {
+      //Construct strings for each row of the matrix
       currLine = new String(matrix[r]);
       System.out.println(currLine);
     }
   }
 
+  // Initializes the matrix to be filled with '.'s
   private static void fillWithPeriods(char[][] matrix) {
 
     for (int r = 0; r < matrix.length; r++) {
@@ -41,22 +46,22 @@ public class DiagonalCipher {
     for (int r = 0; r < matrix.length; r++) {
       for (int c = 0; c < matrix[r].length; c++) {
         myChar = matrix[r][c];
-
+        // Only add non period characters to the output
         if (myChar != '.') {
           currLine += Character.toString(myChar);
         }
 
       }
-      entireText += currLine + " ";
+      entireText += currLine;
     }
     System.out.println(currLine);
 
   }
 
-  private static char[][] buildMatrix(int lineNum, int numRows) {
-    if (lineNum > lineList.size()) {
+  private static void buildMatrix(int lineNum, int numRows) {
+    if (lineNum < 1 || lineNum > lineList.size()) {
       System.out.println("!***Sorry that line is not in the file***! Try again!\n");
-      return null;
+      return;
     }
 
     // Include spaces in the matrix; spaces accounted for with line.length();
@@ -96,8 +101,10 @@ public class DiagonalCipher {
 
     // Print the resulting matrix when finished creating it
     printMatrix(matrix);
+    // Read off the rail-ciphered text
     readMatrix(matrix);
-    return matrix;
+
+    //return matrix;
   }
 
   /* Prompts the user for a line number and number of rows */
@@ -107,9 +114,9 @@ public class DiagonalCipher {
     int numRows;
 
     try {
-      /* Prompt for lineNumber in the file */
+      /* Prompt for line number in the file */
       System.out.print("\nEnter a line number from the file to cipher.\n" +
-        "Or enter 0 to exit\n\n> ");
+        "(The first line in the file is line 1)\nOr enter 0 to exit\n\n> ");
       s = new Scanner(System.in);
       lineNum = s.nextInt();
       if (lineNum == 0) {
@@ -124,6 +131,8 @@ public class DiagonalCipher {
         System.exit(1);
       }
       else {
+        // We have a line in the file and the number of rows
+        // for the desired matrix, so make it!
         buildMatrix(lineNum, numRows);
       }
 
@@ -185,6 +194,8 @@ public class DiagonalCipher {
 
     // The file was read from and the contents of the file are in the lineList
     // ArrayList Object. Begin asking for line numbers and number of rows
+    // The program exits when the user enters a 0 for a line number or for
+    // number of rows in the matrix.
     while (true) {
       cipher();
     }
@@ -193,4 +204,52 @@ public class DiagonalCipher {
   }
 
 }
+
+/* Sample Output:
+  Welcome to DiagonalCipher!
+  Please provide a path to a file to begin.
+
+  > input.txt
+
+  Enter a line number from the file to cipher.
+  (The first line in the file is line 1)
+  Or enter 0 to exit
+
+  > 5
+
+  Enter the number of rows for the matrix.
+  Or enter 0 to exit
+
+  > 4
+
+  W.....e.....p.....t.....n.....?.....?.....?.....
+  .h...v.r...p.e... .o...o.g...b.?...?.?...?.?...?
+  ..a.e... .a...n.d... .p...e.o...?.?...?.?...?.?.
+  ...t.....h.....e.....s.....b.....?.....?.....?..
+
+  Reading off the ciphered text: Weptn???hvrpe oogb??????ae and peo??????thesb???
+
+  Enter a line number from the file to cipher.
+  (The first line in the file is line 1)
+  Or enter 0 to exit
+
+  > 3
+
+  Enter the number of rows for the matrix.
+  Or enter 0 to exit
+
+  > 3
+
+  I...V...A..
+  . .O.E.C.K.
+  ..L... ...E
+
+  Reading off the ciphered text: IVA OECKL E
+
+  Enter a line number from the file to cipher.
+  (The first line in the file is line 1)
+  Or enter 0 to exit
+
+  > 0
+*/
 
---
+++ Security/DiagonalCipher/README.md
@@ -0,0 +1,11 @@
+#How to run:
+1. Have the JDK installed on your computer
+2. Navigate to this project's directory on your computer
+3. Compile the program with 'javac DiagonalCipher.java'
+4. Run the program with 'java DiagonalCipher'
+5. Follow the on screen prompts
+
+
+#Restrictions
+1. Each line in the input file must contain at least one character
+2. A number must be entered when asked for a line number or rows in the matrix
